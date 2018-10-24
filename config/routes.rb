223ac8fe098da5d1f49729e55a923321c033e680
malbazaz <albazaz@gmail.com>
2018-10-23 20:15:39 -0400

    Add route for create campaigns

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
    namespace :api do
-     resources :campaigns, only: [:index, :new, :show, :update]
+     resources :campaigns, only: [:index, :new, :create, :show, :update]
      resources :products, only: [:index, :show]
      resources :products, only: [:show] do 
       resources :campaigns, only: [:index, :new, :show, :update]
