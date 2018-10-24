ca5fff42ea08c8fea5e57f9fa421bb9221e6597d
malbazaz <albazaz@gmail.com>
2018-10-23 20:07:36 -0400

    Correct mistakes on Campaigns controller

--- app/controllers/api/campaigns_controller.rb
+++ app/controllers/api/campaigns_controller.rb
@@ -1,7 +1,5 @@
 class Api::CampaignsController < ApplicationController
     
-    before_action :set_campaign, only: [:show, :update]
-    
     def index
         @campaigns = Product.all 
         render json: @campaigns, status: 200
@@ -9,32 +7,33 @@ class Api::CampaignsController < ApplicationController
 
     def create
         @campaign = Campaign.new(campaign_params)
-        if campaign.save
+        if @campaign.save
             render json: @campaign
         else 
             render json: {message: campaign.errors}, status: 400
+        end
     end 
 
     def show 
-        render json: Campaign.find_by(id: params[:id])
+        @campaign = Campaign.find_by_id(params[:id])
+        render json: @campaign
     end
 
     def update
+        @campaign = Campaign.find_by_id(params[:id])
         if @campaign.update(campaign_params)
             render json: @campaign
         else 
             render json: {message: @campaign.errors}, status: 400
         end 
+    end
 
 
 
     private
-
-    def set_campaign
-        @campaign = Campaign.find_by(id: params[id])
-    end 
-
+    
     def campaign_params
         params.require(:campaign).permit(:product_id, :end_date, :location, :target_qty, :sold_qty)
     end
+
 end
