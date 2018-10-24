8968eff023eb31e206d8f315f1116f63da1826df
malbazaz <albazaz@gmail.com>
2018-10-23 20:11:54 -0400

    Add show and product_params for Products Controller.

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,4 +3,18 @@ class Api::ProductsController < ApplicationController
         @products = Product.all 
         render json: @products, status: 200
     end 
+
+    def show 
+        @product = Product.find_by_id(params[:id])
+        render json: @product
+    end
+
+
+    private
+
+    def product_params
+        params.require(:product).permit(:name, :brand, :description, :category_id, :model, :image_url, :price)
+    end
+
+
 end
